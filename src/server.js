3bfc0a4d591793e70bd7aa3f5dfd02d89e53eb2c
MoonDragon <anznfh97@gmail.com>
2021-07-21 15:03:34 +0900

    practice_02

--- src/server.js
+++ src/server.js
@@ -1,24 +1,19 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRotuer";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const gossipMiddleware = (req, res, next) => {
-    console.log(`Someone is going to: ${req.url}`);
-    next();
-}
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
-const handleHome = (req, res) => {
-    return res.send("I love middlewares");
-}
-
-const handleLogin = (req, res) => {
-    return res.send({message:"you try to login "});
-}
-
-app.get("/", gossipMiddleware, handleHome);
-app.get("/login", handleLogin)
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
