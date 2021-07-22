15eab4178da9c1a42e2a39c7c76c9ca9330a3c6f
MoonDragon <anznfh97@gmail.com>
2021-07-22 12:55:31 +0900

    practice_03

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -8,6 +8,6 @@ globalRouter.get("/", trending);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
 globalRouter.get("/search", search);
-
+const a;
 
 export default globalRouter;
