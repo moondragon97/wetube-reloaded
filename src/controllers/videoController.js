1a4e85f26306c59be9cb8f9f0051e8e2e873b432
MoonDragon <anznfh97@gmail.com>
2021-07-29 16:45:20 +0900

    practice_06

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,10 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-    Video.find({}, (error, videos) => {
-        return res.render("home", {pageTitle: "Home", videos});
-    });
+//Video.find({}, (error, videos) => {});
+
+export const home = async(req, res) => {
+    const videos = await Video.find({});
+    return res.render("home", {pageTitle:"Home", videos});
 };
 export const watch = (req, res) => {
     const {id} = req.params;
@@ -28,3 +29,4 @@ export const postUpload = (req, res) => {
     const {title} = req.body;
     return res.redirect("/");
 }
+  
