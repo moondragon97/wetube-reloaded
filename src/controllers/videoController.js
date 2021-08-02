89c8cbe434b837898b54d27a97bb4c04ceec3553
MoonDragon <anznfh97@gmail.com>
2021-08-02 16:35:42 +0900

    practice_07

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -7,20 +7,38 @@ export const home = async(req, res) => {
     console.log(videos);
     return res.render("home", {pageTitle:"Home", videos});
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
     const {id} = req.params;
-    return res.render("watch", {pageTitle: `Watching`});
-}
-export const getEdit = (req, res) => {
-    const {id} = req.params;
-    res.render("edit", {pageTitle: `Editing: `})
-};
-export const postEdit = (req, res) => {
-    const {id} = req.params;
-    const {title} = req.body; // const title = req.body.title;
-    return res.redirect(`/videos/${id}`);
+    const video = await Video.findById(id);
+    if(video === null){
+        return res.render("404", {pageTitle: "Video not found."});
+    }
+    return res.render("watch", {pageTitle: video.title, video});
 }
 
+export const getEdit = async (req, res) => {
+    const {id} = req.params;
+    const video = await Video.findById(id);
+    if(video === null){
+        return res.render("404", {pageTitle: "Video not found."});
+    }
+    return res.render("edit", {pageTitle: `Edit: ${video.title} `, video});
+};
+
+export const postEdit = async (req, res) => {
+    const {id} = req.params;
+    const {title, description, hashtags} = req.body;
+    const video = await Video.exists({_id:id});
+    if(video === null){
+        return res.render("404", {pageTitle: "Video not found."});
+    }
+    await Video.findByIdAndUpdate(id, {
+        title,description, hashtags: hashtags
+        .split(",")
+        .map(word => word.startsWith('#') ? word : `#${word}`),
+    });
+    return res.redirect(`/videos/${id}`);
+};
 
 export const getUpload = (req, res) => {
     return res.render("upload", {pageTitle: "Upload Video"});
@@ -36,7 +54,10 @@ export const postUpload = async (req, res) => {
         });
         return res.redirect("/");
     }catch(error){
-        return res.render("upload", {pageTitle: "Upload Video", errorMessage: error._message,});
+        return res.render("upload", {
+            pageTitle: "Upload Video",
+            errorMessage: error._message,
+        });
     }
 }
   
