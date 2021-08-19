c3d430fd6d951bd1d1bd0dfc074a7b4a5c792626
MoonDragon <anznfh97@gmail.com>
2021-08-19 17:00:29 +0900

    Github connection

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRotuer";
 import videoRouter from "./routers/videoRouter";
 import { localsMiddleware } from "./middlewares";
-
 
 const app = express();
 const logger = morgan("dev");
@@ -16,9 +16,13 @@ app.use(logger);
 app.use(express.urlencoded({extended: true}));
 
 app.use(session({
-    secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie:{
+        maxAge: 1000000,
+    },
+    store: MongoStore.create({mongoUrl: process.env.DB_URL}),    // 세션 및 로그인 정보를 서버가 아닌 데이터베이스에 저장
 })
 );
 
