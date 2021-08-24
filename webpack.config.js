c6e6832e864ff89fe971fdec98ad536619596ca4
MoonDragon <anznfh97@gmail.com>
2021-08-24 15:47:00 +0900

    install webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+console.log(path.resolve(__dirname, "assets", "js"));
+
+module.exports = {
+    entry: "./src/client/js/main.js",
+    mode: "development",
+    plugins: [new MiniCssExtractPlugin({
+        filename: "css/styles.css"
+    })],
+    output: {
+        filename: "js/main.js",
+        path: path.resolve(__dirname, "assets"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [["@babel/preset-env", { targets: "defaults" }]],
+                    },
+                },
+            },
+            {
+                test: /\.scss$/,
+                // https://github.com/webpack-contrib/sass-loader
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"], 
+            },
+        ]
+    }
+};
