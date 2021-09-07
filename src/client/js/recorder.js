925b9c1a8a6737af35f4f98ec9f667c864bc52a3
MoonDragon <anznfh97@gmail.com>
2021-09-07 17:29:46 +0900

    thumbnail

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,3 +1,4 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
@@ -5,12 +6,50 @@ let stream;
 let recorder;
 let videoFile;
 
-const handleDownload = () => {
+const files = {
+    input: "recording.webm",
+    output: "output.mp4",
+    thumb: "thumbnail.jpg"
+}
+
+const handleDownload = async() => {
+    const ffmpeg = createFFmpeg({log: true});
+    await ffmpeg.load();
+
+    ffmpeg.FS("writeFile", files.input, await fetchFile(videoFile));
+
+    await ffmpeg.run("-i", files.input, "-r", "60", "output.mp4");
+
+    await ffmpeg.run("-i", files.input, "-ss", "00:00:01", "-frames:v", "1", "thumbnail.jpg");
+
+    const mp4File = ffmpeg.FS("readFile", "output.mp4");
+    const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
+
+    const mp4Blob = new Blob([mp4File.buffer], {type: "video/mp4"});
+    const thumbBlob = new Blob([thumbFile.buffer], {type: "image/jpg"});
+
+    const mp4Url = URL.createObjectURL(mp4Blob);
+    const thumbUrl = URL.createObjectURL(thumbBlob);
+
     const a = document.createElement("a");
-    a.href = videoFile;
-    a.download = "MyRecording.webm";
+    a.href = mp4Url;
+    a.download = "MyRecording.mp4";
     document.body.appendChild(a);
     a.click();
+
+    const thumbA = document.createElement("a");
+    thumbA.href = thumbUrl;
+    thumbA.download = "MyThumbnail.jpg";
+    document.body.appendChild(thumbA);
+    thumbA.click();
+
+    ffmpeg.FS("unlink", files.input);
+    ffmpeg.FS("unlink", "output.mp4");
+    ffmpeg.FS("unlink", "thumbnail.jpg");
+    
+    URL.revokeObjectURL(mp4Url);
+    URL.revokeObjectURL(thumbUrl);
+    URL.revokeObjectURL(videoFile);
 }
 
 const handleStop = () => {
--- src/server.js
+++ src/server.js
@@ -27,6 +27,11 @@ app.use(session({
 })
 );
 
+app.use((req, res, next) => {
+    res.header("Cross-Origin-Embedder-Policy", "require-corp");
+    res.header("Cross-Origin-Opener-Policy", "same-origin");
+    next();
+  });
 app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/uploads", express.static("uploads"));
