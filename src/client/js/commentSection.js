8defaf6ce76d6e808755f506f64aa63df7a41a9b
MoonDragon <anznfh97@gmail.com>
2021-09-11 22:36:36 +0900

    comment's delete and edit challenge

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,5 +1,6 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
+const comments = document.querySelectorAll(".video__comment");
 
 
 const addComment = (text, id) => {
@@ -11,12 +12,23 @@ const addComment = (text, id) => {
     icon.className = "fas fa-comment";
     const span = document.createElement("span");
     span.innerText = ` ${text}`;
+    const div = document.createElement("div");
+    div.className = "comment__change";
     const span2 = document.createElement("span");
-    span2.innerText = "❌";
+    span2.innerText = "🖌";
+    span2.className = "comment__edit";
+    const span3 = document.createElement("span");
+    span3.innerText = "❌";
+    span3.className = "comment__delete";
     newComment.appendChild(icon);
     newComment.appendChild(span);
-    newComment.appendChild(span2);
+    newComment.appendChild(div);
+    div.appendChild(span2);
+    div.appendChild(span3);
     videoComments.prepend(newComment);
+        
+    span2.addEventListener("click", handleEdit);
+    span3.addEventListener("click", handleDelete);
 };
 
 const handleSubmit = async (event) => {
@@ -44,6 +56,86 @@ const handleSubmit = async (event) => {
     }
 };
 
+// 댓글 편집
+const handleEdit = async (event) => {
+    event.preventDefault();
+    const comment = event.path[2];
+    const commentId = comment.dataset.id
+    const span = comment.childNodes[1];
+    const icon = comment.childNodes[0];
+    const change = comment.lastChild;
+    const text = span.innerText;
+
+    span.style.display = "none";
+    change.style.display = "none";
+    icon.style.display = "none";
+    const textarea = document.createElement("input");
+    const submitBtn = document.createElement("button");
+    const cancelBtn = document.createElement("button");
+
+    textarea.value = text;
+    submitBtn.innerText = "수정";
+    cancelBtn.innerText = "취소";
+
+    comment.appendChild(textarea);
+    comment.appendChild(submitBtn);
+    comment.appendChild(cancelBtn);
+    
+    const commentInit = () => {
+        textarea.remove();
+        submitBtn.remove();
+        cancelBtn.remove();
+        
+        icon.style.removeProperty("display");
+        change.style.removeProperty("display");
+        span.style.removeProperty("display");
+    };
+
+    cancelBtn.onclick = () => {
+        commentInit();
+        return;
+    }
+
+    submitBtn.onclick = async (event) => {
+        event.preventDefault();
+        span.innerText = textarea.value;
+        commentInit();
+
+        await fetch(`/api/comment/${commentId}/edit`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                text: textarea.value,
+            }),
+        });
+    }
+
+};
+
+// 댓글 삭제
+const handleDelete = async (event) => {
+    event.preventDefault();
+    const comment = event.path[2];
+    console.log(comment);
+    const commentId = comment.dataset.id;
+    const response = await fetch(`/api/comment/${commentId}/delete`,{
+        method: "DELETE",
+    });
+    comment.remove();
+}
+
 if (form){
     form.addEventListener("submit", handleSubmit);
+}
+
+if(comments){
+    for(const commentIndex of comments){
+        const editBtn = commentIndex.querySelector("#edit");
+        const deleteBtn = commentIndex.querySelector("#delete");
+        
+        editBtn.addEventListener("click", handleEdit);
+        deleteBtn.addEventListener("click", handleDelete);
+    }
 }
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -139,4 +139,39 @@ export const createComment = async (req, res) => {
     video.comments.push(comment._id);
     video.save();
     return res.status(201).json({newCommentId: comment._id});
-}
+};
+
+export const deleteComment = async (req, res) => {
+    const{
+        session: {user},
+        params: {id},
+    }=req;
+
+    const comment = await Comment.findById(id).populate("video");
+    if(user._id != comment.owner){
+        return res.sendStatus(404);
+    }
+    const videoId = comment.video._id;
+    const video = await Video.findById(videoId);
+    await video.comments.pop({_id: id});
+    await video.save();
+    await Comment.findByIdAndDelete(id);
+    return res.sendStatus(200);
+};
+
+export const editComment = async (req, res) => {
+    const{
+        session: {user},
+        params: {id},
+        body: {text},
+    }=req;
+    console.log(text);
+    const comment = await Comment.findById(id)
+    if(user._id != comment.owner){
+        return res.sendStatus(404);
+    }
+    await Comment.findByIdAndUpdate(id, {
+        text,
+    });
+    return res.sendStatus(200);
+};
